25d345373b08e2509fb94d31277a1f08ecce17fe
jay98patel <itsj10.jey@gmail.com>
2021-04-21 17:35:59 +0530

    changes in player model and data

--- src/app/modules/ipl-statistics/ipl-player-model.ts
+++ src/app/modules/ipl-statistics/ipl-player-model.ts
@@ -2,10 +2,5 @@ export interface Player{
     id:number,
     playerName:string,
     year:number,
-    playerScore:PlayerScore[]
-}
-
-export interface PlayerScore{
-    playerRun:number;
-    year:number
+    playerScore:number
 }
