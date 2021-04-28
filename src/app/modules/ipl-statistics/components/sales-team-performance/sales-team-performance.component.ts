fb4e7b0ef033af5b269d444f2f7f1030c377c2c1
jay98patel <itsj10.jey@gmail.com>
2021-04-28 14:33:50 +0530

    implemented graph with provided data

--- src/app/modules/ipl-statistics/components/sales-team-performance/sales-team-performance.component.ts
+++ src/app/modules/ipl-statistics/components/sales-team-performance/sales-team-performance.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartDataSets, ChartOptions, ChartType } from 'chart.js';
+import { Color, Label } from 'ng2-charts';
+import { PlayerService } from '../../services/player.service';
+import { SalesByBrands } from '../../ipl-player-model';
 
 @Component({
   selector: 'app-sales-team-performance',
@@ -6,10 +10,61 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sales-team-performance.component.scss']
 })
 export class SalesTeamPerformanceComponent implements OnInit {
+  salesDataSet:ChartDataSets[];
+  salesGraphLabels:Label[];
+  salesGraphLegend:true;
+  salesGraphType: ChartType ='pie';
+  pieChartColor:Color[];
+  heigth:number=400;
+  width:number=300;
 
-  constructor() { }
+  salesGraphOptions:ChartOptions={
+    responsive: true,
+    maintainAspectRatio: false,
+    layout: {
+      padding: {
+        top: 50,
+        right: 10,
+        bottom: 150,
+        left: 0,
+      }
+    },
+    legend: {
+      position: "right",
+      display: true,
+      align: "start",
+      labels: {
+        boxWidth: 12,
+        padding: 5,
+        fontColor: "black"
+      },
+    },
+    plugins: {
+      datalabels: {
+        formatter: () => {
+          return null;
+        },
+      }
+    }
+  }
+  constructor(private reportService:PlayerService) { }
 
   ngOnInit(): void {
+    this.getSalesTeamReport();
+  }
+
+  getSalesTeamReport(){
+    let sale_percent:number[]=[];
+    let productName:string[]=[];
+    this.reportService.getSalesByBrandReport().subscribe((salesByBrands:SalesByBrands[])=>{
+      salesByBrands.map((sales:SalesByBrands)=>{
+
+        productName.push(sales.name);
+        this.salesGraphLabels=[...productName];
+        sale_percent.push(sales.sales_percent);
+        this.salesDataSet=[{data:[...sale_percent]}]
+      })
+    });
   }
 
 }
